875de1303b5096fc7aba481acf3cb238989496b4
Abiodun Jayb <ajiboyeabiodun001@gmail.com>
2023-02-01 09:19:08 +0100

    auth protection

--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -5,10 +5,11 @@ const {
   deleteNote,
   updateNote,
 } = require("../controller/noteController");
+const { protected } = require("../middleware/authMiddleware");
 
 const route = express.Router();
 
-route.route("/").get(getNotes).post(createNote);
-route.route("/:id").put(updateNote).delete(deleteNote);
+route.route("/").get(protected, getNotes).post(protected, createNote);
+route.route("/:id").put(protected, updateNote).delete(protected, deleteNote);
 
 module.exports = route;
